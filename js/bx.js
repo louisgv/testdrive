4b9a5fc409752feb37d26fc3f3fd08ac0e95dc6e
Louis Vichy <lgvichy@gmail.com>
2015-10-20 15:14:03 -0700

    Testing SpeechToSpeech

--- js/bx.js
+++ js/bx.js
@@ -1,27 +1,26 @@
-function makeLine(c, t) {
-  var hLine = t || c;
-  for (var i = 0; i < 9; ++i) {
-    hLine += c;
+function makeLine(a, b, c) {
+  var hLine = b || a;
+  for (var i = 0; i < c; ++i) {
+    hLine += a;
   }
-  hLine += t || c;
+  hLine += b || a;
   console.log(hLine);
 }
 
-function makeBody(c) {
-  var vLine = '';
-  vLine += c;
-  for (var i = 0; i < 9; i++) {
+function makeBody(b, c) {
+  var vLine = b;
+  for (var i = 0; i < c; i++) {
     vLine += ' '
   }
-  vLine += c;
+  vLine += b;
   console.log(vLine);
 }
 
-function makeBox(a, b) {
+function makeBox(a, b, c) {
   // body...
-  makeLine(a, b);
-  for (var i = 0; i < 4; i++) {
-    makeBody(b)
+  makeLine(a, c);
+  for (var i = 0; i < c / 2; i++) {
+    makeBody(b, c)
   }
-  makeLine(a, b);
+  makeLine(a, b, c);
 }
